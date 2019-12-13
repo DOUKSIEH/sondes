48bc067da13212d160aa7792e638d6764a13bf6b
douksieh <douksieha@gmail.com>
2019-12-13 01:07:44 +0100

    modif parsehtml

--- Documents/sondetest/parsehtml.pl
+++ Documents/sondetest/parsehtml.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-
+package Foo;
 use strict;
 use warnings;
 
@@ -7,25 +7,28 @@ use Data::Dumper;
 use Encode;
 use LWP;
 use HTML::TreeBuilder;
+use HTML::FormatText;
 
 my $ua = LWP::UserAgent->new;
-$ua->agent( 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36' );
-my $response = $ua->get( 'http://www.softia.fr' );
+#$ua->agent( 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36' );
+my $response  = $ua->request( HTTP::Request->new( GET => 'http://www.softia.fr' ) );
+#my $response = $ua->get( 'http://www.softia.fr' );
 my $content = $response->content;
 $content = decode( 'utf-8', $content ); # la page est en utf-8, alors on decode l'utf-8
 
 my $tree = HTML::TreeBuilder->new;
 $tree->parse($content);
 
-my @divs = $tree->look_down # on cherche les <div> qui ont class="post article"
+my @hs = $tree->look_down # on cherche les h4 
 (
-  '_tag' => 'div',
-  'class' => 'container'
+  '_tag' => 'h4'
+#   ,
+#   'class' => 'container'
 );
 
-foreach my $div (@divs)
+foreach my $h (@hs)
 {
-  my @ps = $div->find('p'); # on cherche les <p>
+  my @ps = HTML::FormatText->new($h->find('h4')); # on cherche les h
   my $p = shift @ps; # seul le premier <p> nous interesse
-  printf "- \%s\n", $p->as_text;
+  printf "- \%s\n", $p;
 }
